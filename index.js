cb74421652c3be721209092309514cfacaeb4b42
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-05-01 06:36:48 +0100

    cors

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-import express from 'express'
-import dotenv from 'dotenv' 
-import cors from 'cors'
-import './config/db.js'
-import { Router } from './routes/routes.js'
+// import express from 'express'
+// import dotenv from 'dotenv' 
+// import cors from 'cors'
+// import './config/db.js'
+// import { Router } from './routes/routes.js'
 
-const app = express()
-app.use(express.json())
+// const app = express()
+// app.use(express.json())
+// // app.use(cors({
+// //     origin: ["https://contactms-client-gamma.vercel.app/"],
+// //     methods: ["GET", "POST", "PUT", "DELETE"],
+// //     credentials: true
+// // }))
+// // dotenv.config({path: "./config/.env"})
+
+
+
+// app.use('/contactmsyt', Router) 
+
+
+// app.listen(process.env.PORT, () => {
+//     console.log("App is Running")
+// })
+
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import './config/db.js';
+import { Router } from './routes/routes.js';
+
+const app = express();
+app.use(express.json());
 app.use(cors({
-    origin: ["https://contactms-client-gamma.vercel.app/"],
+    origin: ["https://contactms-client-gamma.vercel.app", "https://contactms-api-flame.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}))
-dotenv.config({path: "./config/.env"})
+}));
+dotenv.config({ path: "./config/.env" });
 
-app.use('/contactmsyt', Router) 
-
+// Your route definitions
+app.use('/contactms', Router);
 
 app.listen(process.env.PORT, () => {
-    console.log("App is Running")
-})
+    console.log("App is Running");
+});

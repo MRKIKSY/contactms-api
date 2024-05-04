97672b61d675cf5ac946d279a1e3836d6b41d958
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-05-04 05:20:59 +0100

    wh

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-// import express from 'express'
-// import dotenv from 'dotenv' 
-// import cors from 'cors'
-// import './config/db.js'
-// import { Router } from './routes/routes.js'
+import express from 'express'
+import dotenv from 'dotenv' 
+import cors from 'cors'
+import './config/db.js'
+import { Router } from './routes/routes.js'
 
-// const app = express()
-// app.use(express.json())
-// // app.use(cors({
-// //     origin: ["https://contactms-client-gamma.vercel.app/"],
-// //     methods: ["GET", "POST", "PUT", "DELETE"],
-// //     credentials: true
-// // }))
-// // dotenv.config({path: "./config/.env"})
-
-
-
-// app.use('/contactmsyt', Router) 
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: ["https://contactms-client-gamma.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+dotenv.config({path: "./config/.env"})
 
 
-// app.listen(process.env.PORT, () => {
-//     console.log("App is Running")
-// })
 
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import './config/db.js';
-import { Router } from './routes/routes.js';
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-dotenv.config({ path: "./config/.env" });
-
-// Your route definitions
 app.use('/contactmsyt', Router) 
 
+
 app.listen(process.env.PORT, () => {
-    console.log("App is Running");
-});
+    console.log("App is Running")
+})
+
+
+
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import cors from 'cors';
+// import './config/db.js';
+// import { Router } from './routes/routes.js';
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// dotenv.config({ path: "./config/.env" });
+
+// // Your route definitions
+// app.use('/contactmsyt', Router) 
+
+// app.listen(process.env.PORT, () => {
+//     console.log("App is Running");
+// });
+
+
